56dcebb113753f99baac68cbf2e9362d069cf20d
Bobby Davis <bdavis@core-techs.net>
2022-05-31 13:17:00 -0400

    fixed syntax error in balanced brackets

--- Challenge02/site02.js
+++ Challenge02/site02.js
@@ -1,27 +1,29 @@
 //balanced
 const testbrackets1 = "[()]{}{[()()]()}";
 //balanced
-const testbrakcets2 = `([{}])`
+const testbrackets2 = `([{}])`;
 //not balanced
 const testbrackets3 = "{(})[]";
 //not balanced
 const testbrackets4 = "[(])";
+//not balanced
+const testbrackets5 = "[()";
 
 //driver function used for display and passing values.
 function checkBrackets() {
-    
-    //change the testbracket here. just change it on place.
+
+    //change testbrackets here. makes easire to change in one place.
     let testbrackets = testbrackets1;
+
     //implement isBalanced function. checks if the bracket string is balanced.
     let results = isBalanced(testbrackets);
-    
+
     //used for display no need to change
     let msg = "";
-    
-    if (results == true){
+
+    if (results == true) {
         msg = `Brackets are balanced ==> ${testbrackets}`;
-    }
-    else{
+    } else {
         msg = `Brackets are NOT balanced ==> ${testbrackets}`;
     }
 
@@ -33,7 +35,6 @@ function checkBrackets() {
 //takes an array of strings and returns the longest one. 
 function isBalanced(brackets) {
 
-    return false
+   return false;
 
 }
-
