3a05ad53faef3cf4226660a5e5b74394ca594c4a
Bobby Davis <bdavis@core-techs.net>
2022-05-31 12:23:46 -0400

    add mutiple test brackets

--- Challenge02/site02.js
+++ Challenge02/site02.js
@@ -1,13 +1,19 @@
-const testbrackets = "[()]{}{[()()]()}";
-const testbrackets2 = "{(})[]";
-const testbrackets3 = "[(])";
+//balanced
+const testbrackets1 = "[()]{}{[()()]()}";
+//balanced
+const testbrakcets2 = `([{}])`
+//not balanced
+const testbrackets3 = "{(})[]";
+//not balanced
+const testbrackets4 = "[(])";
 
 //driver function used for display and passing values.
 function checkBrackets() {
     
+    //change the testbracket here. just change it on place.
+    let testbrackets = testbrackets1;
     //implement isBalanced function. checks if the bracket string is balanced.
-    let results = isBalancedB(testbrackets);
-    
+    let results = isBalanced(testbrackets);
     
     //used for display no need to change
     let msg = "";
